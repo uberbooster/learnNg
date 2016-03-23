058179d726d4e9c6e5b509ba1b05777b9eaa0e35
Michael Wohl <mike@gatorriders.com>
2016-03-23 19:55:12 -0400

    created a nav-bar directive to template the nav bar

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,8 @@
 angular.module('learnNg', []);
+
+angular.module('learnNg')
+  .directive('navBar', function(){ //getter syntax
+      return{
+        templateUrl: 'html/templates/nav.html',
+      };
+     });
